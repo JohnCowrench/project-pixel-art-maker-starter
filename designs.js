20128bfa623ae51ad7b62b24a1b16542a7261b7c
JohnCowrench <cowinbathroom@gmail.com>
2017-11-29 01:38:37 +0300

    jQuery first commit

--- designs.js
+++ designs.js
@@ -1,36 +1,42 @@
-var inputRows;
-var inputCols;
-var inputColor = "#000000"
+var gridHeight, gridWidth, gridColor;
 // Select color input
+gridColor = "#000000";
+$('#colorPicker').change(function(){
+    gridColor = $('#colorPicker').val();
+});
 // Select size input
 
-// When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
-    var table = document.getElementById("pixel_canvas");
-    table.innerHTML = "";
-    while(table.rows.length > 0){
-        table.deleteRow(0);
-    }
-    inputRows = document.getElementById("input_height").value;
-    inputCols = document.getElementById("input_width").value;
-    for(var i = 0; i < inputRows; i++){
-        var rowElement = table.insertRow(i);
-        rowElement.setAttribute("class", "row");
-        for(var j = 0; j < inputCols; j++){
-            var cell = rowElement.insertCell(j);
-            cell.addEventListener("click", function(evt){
-                evt.target.style.backgroundColor = document.getElementById("colorPicker").value;
-                this.style.borderColor="#65e9bc";
-                this.style.boxShadow="0 0 5px #65e9bc";
-            });
-        }
-    }
-    return false;
-}
 
-document.getElementById("sizePicker").addEventListener("submit", function(evt){
-    evt.preventDefault();
-    makeGrid();
+$('[type=submit]').click(function(event){
+
+    gridHeight = Number($("input[id=input_height]").val());
+    gridWidth = Number($("input[id=input_width]").val());
+
+    makeGrid(gridHeight, gridWidth);
+    event.preventDefault();
 });
 
+// When size is submitted by the user, call makeGrid()
+function makeGrid(gridHeight, gridWidth) {
+// Your code goes here!
+    console.log("click");
+    if($('#pixel_canvas').find('tr').length){
+        $('#pixel_canvas').find('tr').remove();
+    }
+
+    for(var i = 0; i < gridHeight; i++){
+        $("#pixel_canvas").append("<tr></tr");
+    }
+    for(var i = 0; i < gridWidth; i++){
+        $("#pixel_canvas").find("tr").append("<td></td");
+    }
+
+    $('#colorPicker').change(function(){
+        gridColor = $('#colorPicker').val();
+    });
+
+    $('td').click(function(evt){
+        $( evt.target ).css( 'background', gridColor );
+    });
+}
